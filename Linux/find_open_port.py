62f9b4b8d38559fa207a0fd4a8aa8607c4210070
nikitapov <34512779+nikitapov@users.noreply.github.com>
2022-02-01 11:27:43 +0000

    added min max ports

--- Linux/find_open_port.py
+++ Linux/find_open_port.py
@@ -1,7 +1,20 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("", 0))
-s.listen(1)
-port = s.getsockname()[1]
-s.close()
-print(port)
+
+max_port = 63350
+port = 63300
+
+
+def next_free_port( port=port, max_port=max_port ):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while port <= max_port:
+        try:
+            sock.bind(('', port))
+            sock.close()
+            return port
+        except:
+            port += 1
+    raise IOError('no free ports')
+
+myport = next_free_port() 
+
+print(myport)
